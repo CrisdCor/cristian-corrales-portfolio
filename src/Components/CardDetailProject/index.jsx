b7f47f5fb0ae61bff1a7f0a957e2347593c01f90
Cristian David Corrales Ospina <ccorrales@PASCUALBRAVO.CENTRAL>
2023-08-15 13:25:21 -0500

    Se modifica maquetado de CardDetailProjects

--- src/Components/CardDetailProject/index.jsx
+++ src/Components/CardDetailProject/index.jsx
@@ -1,28 +1,48 @@
 import React from "react";
+import DummieImage from "../../assets/Images/FotoPrueba.png";
+import IconGitHub from "../../assets/Icons/IconGithub.svg";
 import "./styles.css";
 
 const CardDetailProject = ({ name, type, year, repository, url, image }) => {
   return (
-    <article className="card-detail-project">
-      <img className="card-detail-project__image" src={image} alt={name} />
-      <div className="card-detail-project__description">
-        <div className="description__text">
-          <h2 className="text-l">{name}</h2>
-          <p className="text-m text-bold">
-            TIPO: <span className="text-m text-regular">{type}</span>
-          </p>
-          <p className="text-m text-bold">
-            AÑO: <span className="text-m text-regular">{year}</span>
-          </p>
-        </div>
-        <div className="description__links">
-          <a className="link__repository" href={repository}>
-            <img src="" alt="" />
-          </a>
-          <a className="link__url-project" href={url}></a>
-        </div>
+    <div className="detail-project-section">
+      <div className="line-decoration">
+        <span className="line-decoration--circle"></span>
+        <span className="line-decoration--line"></span>
       </div>
-    </article>
+      <article className="detail-project-section__card">
+        <img
+          className="detail-project-section__card--image"
+          src={DummieImage}
+          alt={name}
+        />
+        <div className="description__links">
+          <a className="text-m" href="https://github.com/">
+            Ver Proyecto
+          </a>
+          <a className="link__repository" href="https://github.com/">
+            <img src={IconGitHub} alt="" />
+          </a>
+        </div>
+        <div className="description__text">
+          <h2 className="text-lg text-center">ACADEMIA ALEXANDER</h2>
+          <p className="text-m text-bold text-center">
+            TIPO: <span className="text-m text-regular">Aplicación web.</span>
+          </p>
+          <p className="text-m text-bold text-center">
+            AÑO: <span className="text-m text-regular">2023</span>
+          </p>
+        </div>
+      </article>
+      <p className="detail-project-section--text text-m text-center text-bold">
+        Selecciona cualquier proyecto que quieras ver en detalle
+      </p>
+      <div className="line-decoration line-decoration--right">
+        <span className="line-decoration--line"></span>
+        <span className="line-decoration--circle"></span>
+        <span className="line-decoration--circle"></span>
+      </div>
+    </div>
   );
 };
 
--- src/Pages/projects/index.jsx
+++ src/Pages/projects/index.jsx
@@ -10,22 +10,7 @@ function Projects() {
       <main className="projects">
         <img className="projects__title" src={BgImageProjects} alt="" />
         <AllProjects />
-        <section className="projects__detail">
-          <div className="line-decoration">
-            <span className="line-decoration--circle"></span>
-            <span className="line-decoration--line"></span>
-          </div>
-          <CardDetailProject/>
-          <p className="projects__detail--text text-m text-center text-bold">
-            Selecciona cualquier proyecto que quieras ver en detalle
-          </p>
-          <div className="line-decoration line-decoration--right">
-            <span className="line-decoration--line"></span>
-            <span className="line-decoration--circle"></span>
-            <span className="line-decoration--circle"></span>
-          </div>
-          h
-        </section>
+        <CardDetailProject />
         <figure className="projects--image-decoration">
           <img src="" alt="" />
         </figure>
