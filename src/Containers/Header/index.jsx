9d04fc77b43ad693782ced1471cece9fe9d9f148
Cristian David Corrales Ospina <ccorrales@PASCUALBRAVO.CENTRAL>
2023-09-01 13:02:50 -0500

    Se edita estilos del menú

--- src/Containers/Header/index.jsx
+++ src/Containers/Header/index.jsx
@@ -1,13 +1,22 @@
+import { useContext } from "react";
+import { MenuContext } from "../../Context";
 import ButtonMenu from "../../Components/ButtonMenu";
 import HeaderLogo from "../../Components/HeaderLogo";
 import MainMenu from "../MainMenu";
 import "./styles.css";
 
 const MyHeader = () => {
+  const context = useContext(MenuContext);
+
+  const isClosed = context.isOpenMenu === false;
+  const cardClassName = isClosed
+    ? "animation-menu--close"
+    : "animation-menu--open";
+
   return (
-    <header className="header">
-      <MainMenu />
+    <header className={`header ${cardClassName}`}>
       <div className="header__container">
+      <MainMenu />
         <ButtonMenu />
         <HeaderLogo />
       </div>
